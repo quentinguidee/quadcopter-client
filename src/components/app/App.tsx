2c6c3ee1426bc4feb733283cd875e9d0498b6390
Quentin Guidée <quentin.guidee@gmail.com>
2021-05-10 15:34:28 +0200

    Create App.tsx and basic styles with sass

---
+++ src/components/app/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./sass/App.module.sass";
+
+export default function App() {
+    return <div className={styles.app}></div>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/app/App";
 import reportWebVitals from "./reportWebVitals";
+
+import "./index.sass";
 
 ReactDOM.render(
     <React.StrictMode>
-        <div />
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
