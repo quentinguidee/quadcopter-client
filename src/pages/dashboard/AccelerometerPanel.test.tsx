6a6a15c1ca93a55cfdf1fa90b5f69efb32079a24
Quentin Guidée <quentin.guidee@gmail.com>
2021-05-16 21:21:12 +0200

    Add AccelerometerPanel render test

---
+++ src/pages/dashboard/AccelerometerPanel.test.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import AccelerometerPanel from "./AccelerometerPanel";
+
+test("Render AccelerometerPanel", () => {
+    render(<AccelerometerPanel />);
+});
